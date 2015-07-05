10832fd5abbecf1630ec79f8a45669ed498bfe7d
Adam Seyfarth <adam@seyfarth.name>
2015-07-04 20:51:26 -0500

    Bring in use-package and paredit
    Also try to being in magit.  It isn't compatible with 24.3.
    Put it into a conditional.

--- init.el
+++ init.el
@@ -1,4 +1,23 @@
 (require 'package)
-
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package paredit
+  :ensure t)
+
+; Magit requires emacs version >= 24.4,
+; but Trisquel has 24.3 in the repos.
+(let* ((version-strs (split-string emacs-version "\\."))
+       (version-nums (mapcar 'string-to-number version-strs))
+       (major-version (nth 0 version-nums))
+       (minor-version (nth 1 version-nums)))
+  (if (and (>= major-version 24)
+	   (>= minor-version 4))
+    (use-package magit
+      :ensure t)))
 
