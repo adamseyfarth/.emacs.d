db0a5d286a65d7ce4c377be5ed4555df04aa5e0e
Adam Seyfarth <adam.seyfarth@nrlssc.navy.mil>
2015-07-23 10:35:30 -0500

    Add snippet to use ipython as shell for python
    Taken from: http://www.emacswiki.org/emacs/PythonProgrammingInEmacs

--- init.el
+++ init.el
@@ -23,6 +23,18 @@
 (when (and window-system (not (server-running-p))) (server-start))
 (setq inhibit-startup-screen t)
 
+(require 'python)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
