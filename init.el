661e5b7b15d33760be3acd3580abd643d1dd42cb
Adam Seyfarth <adam.seyfarth@nrlssc.navy.mil>
2015-07-23 11:07:34 -0500

    Use Fixed font, better unicode

--- init.el
+++ init.el
@@ -141,6 +141,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 90 :width normal)))))
+ '(default ((t (:family "Fixed" :foundry "Misc" :slant normal :weight normal :height 120 :width normal)))))
 
 ;;; init.el ends here
